003d3873ebd1127dd329bdad641b84d5a8ab6e05
aalazali <amralazali@gmail.com>
2025-12-11 05:10:04 -0600

    added temporal convolution layer to value function model

--- cot-hallucination-detection/train_value_function3.py
+++ cot-hallucination-detection/train_value_function3.py
@@ -23,10 +23,7 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 from transformers import AutoModel, AutoTokenizer
 
-try:
-    import wandb
-except ImportError:
-    wandb = None
+import wandb
 
 
 # ============================================================================
